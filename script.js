d3523ccfea43dd895f536a13599cf385db311234
Brittney Vaughn <34939860+BrittneyRaquel02@users.noreply.github.com>
2018-01-06 21:39:06 -0500

    Update script.js
    added do, while and for loop for exercise 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 
+//exercise one
 
 function Numbersum (N)
 {
@@ -7,3 +8,30 @@ for (var i= 0; i<=N; i++)
   total +=i
 console.log(sum);
 }
+
+
+//exercise two
+
+    
+    ArrayList list = new ArrayList()
+    var (x = "yes")
+
+    do  {
+       System.out.print("Enter A Word");
+       input=sc.nextLine();
+       list.add(input);
+       count++;
+       }
+
+   while (!input.equals("#"));
+{
+       System.out.print("Want to play again?")
+        input (x===x)
+       }  
+    for ( input (x !=x)
+       strResult = "Case #"+(count-1)+" :"+input;
+}
+
+
+
+//exercise three
